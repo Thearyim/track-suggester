f3e76048b0b23f6121dfcc884b86f587410d3e52
ThearyIm <thearyim@gmail.com>
2019-01-18 19:36:23 -0800

    refactoring Javascript and files.

--- js/scripts.js
+++ js/scripts.js
@@ -4,34 +4,61 @@ function showTrack(track) {
   $("#result").show();
 }
 
-function getTrack(question1Answer, question2Answer, question3Answer, question4Answer, questions5Answer) {
-  var track = "Unsure. The important thing is to get the basic principles of coding down, practice a lot, and be ready to change to another language when your job inevitably does.";
+function getTrack(question1Answer, question2Answer, question3Answer, question4Answer, question5Answer, question6Answer) {
+  var track = "Wow! You might enjoy full-stack development, courses like CSS/Design, Ruby/Rails, PHP, Java and C#.";
+  var backEndDeveloperAnswerCount = 0;
+  var frontEndDeveloperAnswerCount = 0;
 
   if (question1Answer === "yes") {
-    track = "a back-end developer";
-  } else if (question2Answer === "no") {
-    track = "a front-end developer";
-  } else if (question3Answer === "yes") {
-    track = "a back-end developer";
-  } else if (question4Answer === "yes") {
-    track = "a front-end developer";
-  } else if (question4Answer === "no") {
-    track = "unsure. The important thing is to get the basic principles of coding down, practice a lot, and be ready to change to another language when your job inevitably does.";
+    backEndDeveloperAnswerCount++;
+  }
+  else {
+    frontEndDeveloperAnswerCount++;
   }
 
-  // if (question1Answer === "yes") {
-  //   track = "Front End1";
-  // } else if (question2Answer === "yes") {
-  //   track = "Front End2";
-  // } else if (question3Answer === "yes") {
-  //   track = "Front End3";
-  // } else if (question4Answer === "yes") {
-  //   track = "Front End4";
-  // } else if (question5Answer === "yes") {
-  //   track = "Front End5";
-  // } else if (question5Answer === "no") {
-  //   track = "Unsure";
-  // }
+  if (question2Answer === "yes"){
+    frontEndDeveloperAnswerCount++;
+  }
+  else {
+    backEndDeveloperAnswerCount++;
+  }
+
+  if (question3Answer === "yes") {
+    frontEndDeveloperAnswerCount++;
+  }
+  else {
+    backEndDeveloperAnswerCount++;
+  }
+
+  if (question4Answer === "yes") {
+    frontEndDeveloperAnswerCount++;
+  }
+  else {
+    backEndDeveloperAnswerCount++;
+  }
+
+  if (question5Answer === "yes") {
+    backEndDeveloperAnswerCount++;
+  }
+  else {
+    frontEndDeveloperAnswerCount++;
+  }
+
+  if (question6Answer === "yes") {
+    frontEndDeveloperAnswerCount++;
+  }
+  else {
+    backEndDeveloperAnswerCount++;
+  }
+
+  if (frontEndDeveloperAnswerCount > backEndDeveloperAnswerCount)
+  {
+     track = "You might like front-end development, courses like CSS/Design.";
+  }
+  else if (backEndDeveloperAnswerCount > frontEndDeveloperAnswerCount)
+  {
+    track = "You might like back-end development, courses like Ruby/Rails, PHP, Java and C#.";
+  }
 
   return track;
 }
@@ -43,7 +70,8 @@ $(document).ready(function(){
     var question3 = $("input:radio[name=question3-answer]:checked").val();
     var question4 = $("input:radio[name=question4-answer]:checked").val();
     var question5 = $("input:radio[name=question5-answer]:checked").val();
-    var track = getTrack(question1, question2, question3, question4, question5);
+    var question6 = $("input:radio[name=question6-answer]:checked").val();
+    var track = getTrack(question1, question2, question3, question4, question5, question6);
 
     showTrack(track);
     event.preventDefault();
